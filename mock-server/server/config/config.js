acb47f0bbf5026a7cdf6e7508ea3dc8061fc925a
Sheehan Toufiq <sheehantoufiq@gmail.com>
2022-02-15 02:31:16 -0500

    auth, frontend screenshots

--- mock-server/server/config/config.js
+++ mock-server/server/config/config.js
@@ -2,8 +2,8 @@
 
 module.exports = {
 	version: '1.0-dev',
-	database: 'mongodb+srv://test:test@cluster0.2kbxr.mongodb.net/frigg-mock-database?retryWrites=true&w=majority',
-	secret: 'secretsecretscret',
+	database: process.env.MONGO_URI,
+	secret: process.env.SECRET,
 };
 
 
