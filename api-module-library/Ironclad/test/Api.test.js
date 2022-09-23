8a7546a97add23baacb14ed713eb4bedf00cec51
Jonathan Moore <jonathan.moore6247@gmail.com>
2022-09-23 12:58:45 -0400

    API class tests passing

--- api-module-library/Ironclad/test/Api.test.js
+++ api-module-library/Ironclad/test/Api.test.js
@@ -11,7 +11,7 @@ describe('Ironclad API class', () => {
         api_key: 'FsnPyagbghJPoZYf618X9F8EJXZUyZKRYsfbGr6kciq4',
     });
 
-    describe('Webhooks', () => {
+    describe.only('Webhooks', () => {
         it('should list all webhooks', async () => {
             const response = await api.listWebhooks();
             expect(response).to.have.property('page');
@@ -19,6 +19,24 @@ describe('Ironclad API class', () => {
             expect(response).to.have.property('list');
         });
 
+        it('should create a webhook', async () => {
+            const events = [
+                'workflow_cancelled'
+            ];
+            const targetURL = 'https://webhook.site/d895e13f-ccee-47fc-9ba9-fc41a8f7fb5e';
+            const response = await api.createWebhook(events, targetURL);
+            console.log(response);
+            expect(response).to.have.property('id');
+            expect(response).to.have.property('events');
+            expect(response).to.have.property('targetURL');
+            expect(response).to.have.property('companyId');
+        });
+
+        it('should delete a webhook', async () => {
+            const id = '632de3658a95cb469df741e9';
+            const response = await api.deleteWebhook(id);
+            expect(response.status).equal(204)
+        })
     });
 });
 
