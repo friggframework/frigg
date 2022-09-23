1d0c059a7d98716e9fd746dee4072f33b68292f3
Jonathan Moore <jonathan.moore6247@gmail.com>
2022-09-23 10:09:22 -0400

    Added this.URLs

--- api-module-library/Ironclad/Api.js
+++ api-module-library/Ironclad/Api.js
@@ -4,26 +4,14 @@ const { FetchError } = require('@friggframework/errors/FetchError');
 class IroncladAPI extends OAuth2Base {
     constructor(params) {
         super(params);
+        this.baseUrl = 'https://demo.ironcladapp.com';
 
-        this.baseUrl = '';
-
-        this.client_id = process.env.IRONCLAD_CLIENT_ID;
-        this.client_secret = process.env.IRONCLAD_CLIENT_SECRET;
-        this.redirect_uri = `${process.env.IRONCLAD_REDIRECT_URI}`;
-        this.scopes = process.env.IRONCLAD_SCOPE;
+        this.api_key = get(params, 'api_key', null);
 
         this.URLs = {
-            me: '/me',
-            exampleEndpont: '/endpoint',
+            webhooks: '/public/api/v1/webhooks',
+            webhookByID: (webhookId) => `/public/api/v1/webhooks/${webhookId}`
         };
-
-        this.authorizationUri = encodeURI(
-            `https://app.example.com/oauth/authorize?response_type=code&client_id=${this.client_id}&redirect_uri=${this.redirect_uri}`
-        );
-        this.tokenUri = 'https://app.example.com/oauth/token';
-
-        this.access_token = get(params, 'access_token', null);
-        this.refresh_token = get(params, 'refresh_token', null);
     }
 
     async getTokenFromCode(code) {
