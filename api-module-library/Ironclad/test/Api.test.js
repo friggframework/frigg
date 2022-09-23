4a43ce2d6b076be3856fef9d91b68b18f032ff4f
Jonathan Moore <jonathan.moore6247@gmail.com>
2022-09-23 12:32:52 -0400

    Lists all webhooks

--- api-module-library/Ironclad/test/Api.test.js
+++ api-module-library/Ironclad/test/Api.test.js
@@ -3,19 +3,22 @@ const should = chai.should();
 //const Authenticator = require('../../../../test/utils/Authenticator');
 const { Api } = require('../api');
 
-const TestUtils = require('../../../../test/utils/TestUtils');
+//const TestUtils = require('../../../../test/utils/TestUtils');
 const { expect } = require('chai');
 
 describe('Ironclad API class', () => {
     const api = new Api({
-        api_key: process.env.IRONCLAD_API_KEY,
+        api_key: 'FsnPyagbghJPoZYf618X9F8EJXZUyZKRYsfbGr6kciq4',
     });
 
     describe('Webhooks', () => {
-        it.only('should list all webhooks', async () => {
+        it('should list all webhooks', async () => {
             const response = await api.listWebhooks();
-            console.log(response);
+            expect(response).to.have.property('page');
+            expect(response).to.have.property('pageSize')
+            expect(response).to.have.property('list');
         });
+
     });
 });
 
