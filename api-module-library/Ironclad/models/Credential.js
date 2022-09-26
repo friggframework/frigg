2a3bd44a7c7e037c7b5b6aa413ff2e7f01fd8839
Jonathan Moore <jonathan.moore6247@gmail.com>
2022-09-26 09:46:31 -0400

    Updated Credential

---
+++ api-module-library/Ironclad/models/Credential.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const { Credential: Parent } = require('@friggframework/module-plugin');
+
+const schema = new mongoose.Schema({
+    api_key: {
+        type: String,
+        trim: true,
+        lhEncrypt: true,
+    },
+    /* api_url: {
+        type: String,
+        required: true,
+    }, */
+});
+
+const name = 'IroncladCredential';
+const Credential = 
+    Parent.discriminators?.[name] || Parent.discriminators(name, schema);
+module.exports = { Credential };
