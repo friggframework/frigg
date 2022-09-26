de11c6a8d7b1b70f1b728e143870a394a6f41099
Jonathan Moore <jonathan.moore6247@gmail.com>
2022-09-26 13:28:59 -0400

    Cleaned up Manager

--- api-module-library/Ironclad/Manager.js
+++ api-module-library/Ironclad/Manager.js
@@ -82,16 +82,6 @@ class Manager extends ModuleManager {
         };
     }
 
-    async getEntityOptions() {
-        // No entity options to get. Probably won't even hit this
-        return [];
-    }
-
-    async findOrCreateEntity(data) {
-        // Creating entity in send with credential creation... Just do a find
-        return this.entityMO.getByUserId(data.userId);
-    }
-
     async deauthorize() {
         // wipe api connection
         this.api = new Api();
@@ -102,66 +92,6 @@ class Manager extends ModuleManager {
             await this.credentialMO.delete(entity.credential);
             entity.credential = undefined;
             await entity.save();
-        }
-    }
-
-    async receiveNotification(notifier, delegateString, object = null) {
-        if (notifier instanceof Api) {
-            if (delegateString === this.api.DLGT_TOKEN_UPDATE) {
-                // todo update the database
-                const userDetails = await this.api.getTokenIdentity();
-                const updatedToken = {
-                    user: this.userId.toString(),
-                    access_token: this.api.access_token,
-                    refresh_token: this.api.refresh_token,
-                    auth_is_valid: true,
-                };
-
-                Object.keys(updatedToken).forEach(
-                    (k) => updatedToken[k] == null && delete updatedToken[k]
-                );
-
-                let entity = await this.entityMO.getByUserId(this.userId);
-                if (!entity) {
-                    entity = await this.entityMO.create({
-                        user: this.userId,
-                        externalId: userDetails.id,
-                        name: userDetails.name,
-                    });
-                }
-
-                let { credential } = entity;
-                if (!credential) {
-                    credential = await this.credentialMO.create(updatedToken);
-                } else {
-                    credential = await this.credentialMO.update(
-                        credential,
-                        updatedToken
-                    );
-                }
-                await this.entityMO.update(entity.id, { credential });
-            }
-            if (delegateString === this.api.DLGT_TOKEN_DEAUTHORIZED) {
-                await this.deauthorize();
-            }
-            if (delegateString === this.api.DLGT_INVALID_AUTH) {
-                return this.markCredentialsInvalid();
-            }
-        }
-    }
-
-    async mark_credentials_invalid() {
-        let credentials = await this.credentialMO.list({ user: this.userId });
-        if (credentials.length === 1) {
-            return await this.credentialMO.update(credentials[0]._id, {
-                auth_is_valid: false,
-            });
-        } else if (credentials.length > 1) {
-            throw new Error('User has multiple credentials???');
-        } else if (credentials.length === 0) {
-            throw new Error(
-                'How are we marking nonexistant credentials invalid???'
-            );
         }
     }
 }
