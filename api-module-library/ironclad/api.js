464302ef4af86d4de4fcce8680e57f852d020098
Jonathan Moore <jonathan.moore6247@gmail.com>
2022-09-27 11:44:32 -0400

    Tests passing

---
+++ api-module-library/ironclad/api.js
@@ -0,0 +1,79 @@
+const { ApiKeyRequester } = require('@friggframework/module-plugin');
+const { get } = require('@friggframework/assertions');
+
+class Api extends ApiKeyRequester {
+    constructor(params) {
+        super(params);
+
+        this.API_KEY_NAME = 'Bearer';
+        this.API_KEY_VALUE = get(params, 'apiKey', null);
+        this.baseUrl = 'https://demo.ironcladapp.com';
+
+        this.URLs = {
+            webhooks: '/public/api/v1/webhooks',
+            webhookByID: (webhookId) => `/public/api/v1/webhooks/${webhookId}`
+        };
+    }
+
+    async addAuthHeaders(headers) {
+        if (this.API_KEY_VALUE) {
+            headers.Authorization = `Bearer ${this.API_KEY_VALUE}`;
+        }
+
+        return headers;
+    }
+
+    async listWebhooks() {
+        const options = {
+            url: this.baseUrl + this.URLs.webhooks,
+        };
+        const response = await this._get(options);
+        return response;
+    }
+
+    async createWebhook(events, targetURL) {
+        const options = {
+            url: this.baseUrl + this.URLs.webhooks,
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: {
+                events,
+                targetURL
+            }
+        };
+        const response = await this._post(options);
+        return response;
+    }
+
+    async updateWebhook(webhookId, events = null, targetURL = null) {
+        const options = {
+            url: this.baseUrl + this.URLs.webhookByID(webhookId),
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: {}
+        }
+
+        if (events.length > 0) {
+            options.body.events = events;
+        }
+
+        if (targetURL) {
+            options.body.targetURL = targetURL;
+        }
+
+        const response = await this._patch(options);
+        return response;
+
+    }
+    async deleteWebhook(webhookId) {
+        const options = {
+            url: this.baseUrl + this.URLs.webhookByID(webhookId)
+        }
+        const response = await this._delete(options);
+        return response;
+    }
+}
+
+module.exports = { Api };
---
+++ api-module-library/ironclad/test/api.test.js
@@ -0,0 +1,50 @@
+const chai = require('chai');
+const should = chai.should();
+const { Api } = require('../api');
+require('dotenv').config()
+const { expect } = require('chai');
+
+describe('Ironclad API class', () => {
+    const api = new Api({
+        apiKey: process.env.IRONCLAD_API_KEY,
+    });
+
+    describe('Webhooks', () => {
+        let webhookID
+        it('should list all webhooks', async () => {
+            const response = await api.listWebhooks();
+            expect(response).to.have.property('page');
+            expect(response).to.have.property('pageSize')
+            expect(response).to.have.property('list');
+        });
+
+        it('should create a webhook', async () => {
+            const events = [
+                'workflow_launched'
+            ];
+            const targetURL = process.env.IRONCLAD_WEBHOOK_URL;
+            const response = await api.createWebhook(events, targetURL);
+            expect(response).to.have.property('id');
+            expect(response).to.have.property('events');
+            expect(response).to.have.property('targetURL');
+            expect(response).to.have.property('companyId');
+            webhookID = response.id;
+        });
+
+        it('should update a webhook', async () => {
+            const events = [
+                'workflow_cancelled'
+            ];
+            const response = await api.updateWebhook(webhookID, events)
+            expect(response).to.have.property('id');
+            expect(response).to.have.property('events');
+            expect(response).to.have.property('targetURL');
+            expect(response).to.have.property('companyId');
+        })
+
+        it('should delete a webhook', async () => {
+            const response = await api.deleteWebhook(webhookID);
+            expect(response.status).equal(204)
+        })
+    });
+});
