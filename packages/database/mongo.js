fdddd83145db20ac4869741364418124ce5abb73
Sheehan Toufiq Khan <sheehantoufiq@gmail.com>
2022-12-06 16:33:11 -0500

    fix modules to @friggframework

--- packages/database/mongo.js
+++ packages/database/mongo.js
@@ -2,9 +2,9 @@
 // https://dev.to/adnanrahic/building-a-serverless-rest-api-with-nodejs-and-mongodb-43db
 // https://mongoosejs.com/docs/lambda.html
 // https://www.mongodb.com/blog/post/optimizing-aws-lambda-performance-with-mongodb-atlas-and-nodejs
-const { Encrypt } = require('../encrypt');
+const { Encrypt } = require('@friggframework/encrypt');
 const mongoose = require('mongoose'); // TODO maybe pass in / see what best way to make sure we have the same mongoose instance as the including package (something like peerDependencies but not deprecated)
-const { debug, flushDebugLog } = require('../logs');
+const { debug, flushDebugLog } = require('@friggframework/logs');
 
 mongoose.plugin(Encrypt);
 mongoose.set('applyPluginsToDiscriminators', true); // Needed for LHEncrypt
--- packages/logs/logger.test.js
+++ packages/logs/logger.test.js
@@ -3,7 +3,7 @@ const sinon = require('sinon');
 const {
     overrideEnvironment,
     restoreEnvironment,
-} = require('../test-environment');
+} = require('@friggframework/test-environment');
 
 /* eslint-disable no-console */
 
