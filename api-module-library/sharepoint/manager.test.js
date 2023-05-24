26310856c932234a85f1f62362334a6d83558441
roboli <robertooliveros@mac.com>
2023-05-24 16:10:34 -0600

    Test processAuthorizationCallback when error thrown

--- api-module-library/sharepoint/manager.test.js
+++ api-module-library/sharepoint/manager.test.js
@@ -98,62 +98,125 @@ describe(`Should fully test the ${config.label} Manager`, () => {
     });
 
     describe('#processAuthorizationCallback', () => {
-        let manager;
+        describe('Perform authorization', () => {
+            const baseUrl = 'https://graph.microsoft.com/v1.0';
+            let authScope, sitesScope, userScope;
+            let manager;
 
-        beforeEach(async () => {
-            manager = await Manager.getInstance({
-                userId: new mongoose.Types.ObjectId(),
+            beforeEach(async () => {
+                manager = await Manager.getInstance({
+                    userId: new mongoose.Types.ObjectId(),
+                });
+
+                const body = querystring.stringify({
+                    grant_type: 'authorization_code',
+                    client_id: 'sharepoint_client_id_test',
+                    client_secret: 'sharepoint_client_secret_test',
+                    redirect_uri: 'http://redirect_uri_test/microsoft-sharepoint',
+                    scope: 'sharepoint_scope_test',
+                    code: 'test'
+                });
+
+                authScope = nock('https://login.microsoftonline.com')
+                    .post('/common/oauth2/v2.0/token', )
+                    .reply(200, {
+                        access_token: 'access_token',
+                        refresh_token: 'refresh_token',
+                        expires_in: 'expires_in'
+                    });
+
+                sitesScope = nock(baseUrl)
+                    .get('/sites?search=*')
+                    .reply(200, {
+                        sites: 'sites'
+                    });
+
+                userScope = nock(baseUrl)
+                    .get('/me')
+                    .reply(200, {
+                        id: 'id',
+                        displayName: 'displayName',
+                        userPrincipalName: 'userPrincipalName'
+                    });
+            });
+
+            it('should return an entity_id, credential_id, and type for successful auth', async () => {
+                const params = { code: 'code ' };
+
+                const res = await manager.processAuthorizationCallback(params);
+                expect(res).toBeDefined();
+                expect(res.entity_id).toBeDefined();
+                expect(res.credential_id).toBeDefined();
+                expect(res.type).toEqual(config.name);
+
+                expect(authScope.isDone()).toBe(true);
+                expect(sitesScope.isDone()).toBe(true);
+                expect(userScope.isDone()).toBe(true);
             });
         });
 
-        const baseUrl = 'https://graph.microsoft.com/v1.0';
-        let authScope, sitesScope, userScope;
+        describe('Perform authorization to wrong auth URL', () => {
+            const baseUrl = 'https://graph.microsoft.com/v1.0';
+            let authScope, sitesScope, userScope;
+            let manager;
 
-        beforeEach(() => {
-            const body = querystring.stringify({
-                grant_type: 'authorization_code',
-                client_id: 'sharepoint_client_id_test',
-                client_secret: 'sharepoint_client_secret_test',
-                redirect_uri: 'http://redirect_uri_test/microsoft-sharepoint',
-                scope: 'sharepoint_scope_test',
-                code: 'test'
+            beforeEach(async () => {
+                // Silent error log when doing Auth request
+                jest.spyOn(console, 'error').mockImplementation(() => {});
+
+                manager = await Manager.getInstance({
+                    userId: new mongoose.Types.ObjectId(),
+                });
+
+                const body = querystring.stringify({
+                    grant_type: 'authorization_code',
+                    client_id: 'sharepoint_client_id_test',
+                    client_secret: 'sharepoint_client_secret_test',
+                    redirect_uri: 'http://redirect_uri_test/microsoft-sharepoint',
+                    scope: 'sharepoint_scope_test',
+                    code: 'test'
+                });
+
+                authScope = nock('https://login.microsoftonline.com')
+                    .post('/common/oauth2/v2.0/token', )
+                    .reply(200, {
+                        access_token: 'access_token',
+                        refresh_token: 'refresh_token',
+                        expires_in: 'expires_in'
+                    });
+
+                sitesScope = nock(baseUrl)
+                    .get('/sites?search=WRONG')
+                    .reply(200, {
+                        sites: 'sites'
+                    });
+
+                userScope = nock(baseUrl)
+                    .get('/me')
+                    .reply(200, {
+                        id: 'id',
+                        displayName: 'displayName',
+                        userPrincipalName: 'userPrincipalName'
+                    });
             });
 
-            authScope = nock('https://login.microsoftonline.com')
-                .post('/common/oauth2/v2.0/token', )
-                .reply(200, {
-                    access_token: 'access_token',
-                    refresh_token: 'refresh_token',
-                    expires_in: 'expires_in'
-                });
+            afterEach(() => {
+                jest.clearAllMocks();
+            });
 
-            sitesScope = nock(baseUrl)
-                .get('/sites?search=*')
-                .reply(200, {
-                    sites: 'sites'
-                });
+            it('should throw auth error', async () => {
+                const params = { code: 'code ' };
 
-            userScope = nock(baseUrl)
-                .get('/me')
-                .reply(200, {
-                    id: 'id',
-                    displayName: 'displayName',
-                    userPrincipalName: 'userPrincipalName'
-                });
-        });
+                try {
+                    await manager.processAuthorizationCallback(params);
+                } catch(e) {
+                    expect(e).toEqual(new Error('Authentication failed'));
+                }
 
-        it('should return an entity_id, credential_id, and type for successful auth', async () => {
-            const params = { code: 'code ' };
-
-            const res = await manager.processAuthorizationCallback(params);
-            expect(res).toBeDefined();
-            expect(res.entity_id).toBeDefined();
-            expect(res.credential_id).toBeDefined();
-            expect(res.type).toEqual(config.name);
-
-            expect(authScope.isDone()).toBe(true);
-            expect(sitesScope.isDone()).toBe(true);
-            expect(userScope.isDone()).toBe(true);
+                expect(authScope.isDone()).toBe(true);
+                expect(sitesScope.isDone()).toBe(false);
+                expect(userScope.isDone()).toBe(false);
+            });
         });
     });
 });
