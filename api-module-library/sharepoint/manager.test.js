56b47210b854947de52ac211900c9323d21df6ec
roboli <robertooliveros@mac.com>
2023-05-24 15:39:49 -0600

    Improve getInstance test

--- api-module-library/sharepoint/manager.test.js
+++ api-module-library/sharepoint/manager.test.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const nock = require('nock');
 const querystring = require('querystring');
 const Manager = require('./manager');
+const { Entity } = require('./models/entity');
+const { Credential } = require('./models/credential');
 const config = require('./defaultConfig.json');
 
 describe(`Should fully test the ${config.label} Manager`, () => {
@@ -19,17 +21,53 @@ describe(`Should fully test the ${config.label} Manager`, () => {
     });
 
     describe('#getInstance', () => {
-        let manager;
+        describe('Create new instance', () => {
+            let manager;
 
-        beforeEach(async () => {
-            manager = await Manager.getInstance({
-                userId: new mongoose.Types.ObjectId(),
+            beforeEach(async () => {
+                manager = await Manager.getInstance({
+                    userId: new mongoose.Types.ObjectId(),
+                });
+            });
+
+            it('can create an instance of Module Manger', async () => {
+                expect(manager).toBeDefined();
+                expect(manager.api).toBeDefined();
             });
         });
 
-        it('can create an instance of Module Manger', async () => {
-            expect(manager).toBeDefined();
-            expect(manager.api).toBeDefined();
+        describe('Create new instance with entity Id', () => {
+            let manager;
+
+            beforeEach(async () => {
+                const userId = new mongoose.Types.ObjectId();
+
+                const creden = await Credential.create({
+                    user: userId,
+                    accessToken: 'accessToken',
+                    refreshToken: 'refreshToken',
+                    auth_is_valid: true,
+                });
+
+                const enti = await Entity.create({
+                    credential: creden.id,
+                    user: userId,
+                    name: 'name',
+                    externalId: 'externalId',
+                });
+
+                manager = await Manager.getInstance({
+                    entityId: enti.id,
+                    userId
+                });
+            });
+
+            it('can create an instance of Module Manger with credentials', async () => {
+                expect(manager).toBeDefined();
+                expect(manager.api).toBeDefined();
+                expect(manager.api.access_token).toEqual('accessToken');
+                expect(manager.api.refresh_token).toEqual('refreshToken');
+            });
         });
     });
 
