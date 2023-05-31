2832fb0bfb76c15d7472cf8ddfbcfc7a68d3cb0a
roboli <robertooliveros@mac.com>
2023-05-31 10:20:20 -0600

    Test getInstance method [ci skip]

--- api-module-library/frontify/manager.test.js
+++ api-module-library/frontify/manager.test.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const Manager = require('./manager');
+const { Entity } = require('./models/entity');
+const { Credential } = require('./models/credential');
 const config = require('./defaultConfig.json');
 
 describe(`Should fully test the ${config.label} Manager`, () => {
@@ -19,6 +21,57 @@ describe(`Should fully test the ${config.label} Manager`, () => {
     describe('#getName', () => {
         it('should return manager name', () => {
             expect(Manager.getName()).toEqual('frontify');
+        });
+    });
+
+    describe('#getInstance', () => {
+        describe('Create new instance', () => {
+            let manager;
+
+            beforeEach(async () => {
+                manager = await Manager.getInstance({
+                    userId: new mongoose.Types.ObjectId(),
+                });
+            });
+
+            it('can create an instance of Module Manger', async () => {
+                expect(manager).toBeDefined();
+                expect(manager.api).toBeDefined();
+            });
+        });
+
+        describe('Create new instance with entity Id', () => {
+            let manager;
+
+            beforeEach(async () => {
+                const userId = new mongoose.Types.ObjectId();
+
+                const creden = await Credential.create({
+                    user: userId,
+                    accessToken: 'accessToken',
+                    refreshToken: 'refreshToken',
+                    auth_is_valid: true,
+                });
+
+                const enti = await Entity.create({
+                    credential: creden.id,
+                    user: userId,
+                    name: 'name',
+                    externalId: 'externalId',
+                });
+
+                manager = await Manager.getInstance({
+                    entityId: enti.id,
+                    userId
+                });
+            });
+
+            it('can create an instance of Module Manger with credentials', async () => {
+                expect(manager).toBeDefined();
+                expect(manager.api).toBeDefined();
+                expect(manager.api.access_token).toEqual('accessToken');
+                expect(manager.api.refresh_token).toEqual('refreshToken');
+            });
         });
     });
 });
