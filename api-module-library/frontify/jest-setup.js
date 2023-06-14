8117cbe43d0fb5202a52d38532ecf519aa174ab0
roboli <robertooliveros@mac.com>
2023-06-14 09:54:48 -0600

    Fix renaming REDIRECT_URI when testing [ci skip]

--- api-module-library/frontify/jest-setup.js
+++ api-module-library/frontify/jest-setup.js
@@ -5,7 +5,7 @@ const parsed = {
     FRONTIFY_SCOPE: 'frontify_scope_test',
     FRONTIFY_CLIENT_ID: 'frontify_client_id_test',
     FRONTIFY_CLIENT_SECRET: 'frontify_client_secret_test',
-    FRONTIFY_REDIRECT_URI: 'http://redirect_uri_test'
+    REDIRECT_URI: 'http://redirect_uri_test'
 };
 
 dotenv.populate(process.env, parsed);
