bf6b185744390115682cf701eb53cb03a5ec5c12
roboli <robertooliveros@mac.com>
2023-08-12 16:08:52 -0600

    Push comment hoping for module to be released!

--- api-module-library/google-drive/api.js
+++ api-module-library/google-drive/api.js
@@ -92,6 +92,7 @@ class Api extends OAuth2Requester {
 
     async getFileData(fileId) {
         // Return full response to have access to stream in response.body
+        // thanks to alt=media query param
         const options = {
             url: this.baseUrl + this.URLs.fileById(fileId),
             query: {
