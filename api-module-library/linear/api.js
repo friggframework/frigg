2804e7b7996962a56eafc4bbde3db271b01ed8e0
michael webber <michaelryanwebber@gmail.com>
2023-09-18 17:11:55 -0400

    add linear api module

---
+++ api-module-library/linear/api.js
@@ -0,0 +1,53 @@
+const { OAuth2Requester } = require('@friggframework/module-plugin');
+const { get } = require('@friggframework/assertions');
+const { LinearClient } = require('@linear/sdk');
+
+class Api extends OAuth2Requester {
+    constructor(params) {
+        super(params);
+        this.actor = get(params, 'actor', 'application');
+        this.access_token = get(params, 'access_token', null);
+        this.authorizationUri = encodeURI(
+            `https://linear.app/oauth/authorize?response_type=code` +
+            `&scope=${this.scope}` +
+            `&client_id=${this.client_id}` +
+            `&redirect_uri=${this.redirect_uri}` +
+            `&state=${this.state}`+
+            `&actor=${this.actor}`
+        );
+        this.tokenUri = 'https://api.linear.app/oauth/token';
+    }
+
+    async getTokenIdentity() {
+        const user = await this.getUser();
+        const org = await this.getOrganization();
+        return {identifier: org.id, name: user.name};
+    }
+
+    async getUser() {
+        const client = new LinearClient({accessToken: this.access_token});
+        return client.viewer;
+    }
+
+    async getOrganization() {
+        const client = new LinearClient({accessToken: this.access_token});
+        return client.organization;
+    }
+
+    async getUsers() {
+        const client = new LinearClient({accessToken: this.access_token});
+        return (await client.users()).nodes;
+    }
+
+    async getUserIssues(user){
+        return user.assignedIssues();
+    }
+
+    async getProjects(){
+        const client = new LinearClient({accessToken: this.access_token});
+        return (await client.projects()).nodes;
+    }
+
+}
+
+module.exports = { Api };
---
+++ api-module-library/linear/models/credential.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const { Credential: Parent } = require('@friggframework/module-plugin');
+const schema = new mongoose.Schema({
+    access_token: {
+        type: String,
+        trim: true,
+        lhEncrypt: true,
+    },
+    expires_at: { type: Number },
+});
+const name = 'LinearCredential';
+const Credential = Parent.discriminators?.[name] || Parent.discriminator(name, schema);
+module.exports = { Credential };
