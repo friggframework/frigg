e663b0c8df178c1d37e11d5a03c8c80f0f6362df
Michael Webber <michaelryanwebber@gmail.com>
2023-12-18 18:45:20 -0500

    add back lock file

--- api-module-library/hubspot/models/entity.js
+++
@@ -1,9 +0,0 @@
-const { mongoose } = require('@friggframework/database/mongoose');
-const { Entity: Parent } = require('@friggframework/module-plugin');
-
-const schema = new mongoose.Schema({});
-
-const name = 'HubSpotEntity';
-const Entity =
-    Parent.discriminators?.[name] || Parent.discriminator(name, schema);
-module.exports = { Entity };
