092d971303b8507fd6ccb3956382f8ee2cbd2fc9
Michael Webber <michaelryanwebber@gmail.com>
2024-01-18 13:52:42 -0500

    rough test for Auther eventually this will be easy to swap out different api modules/definitions and confirm api param and db model

---
+++ packages/module-plugin/test/mock-api/api.js
@@ -0,0 +1,29 @@
+const { get } = require('@friggframework/assertions');
+const { OAuth2Requester } = require('@friggframework/module-plugin');
+
+class Api extends OAuth2Requester {
+    constructor(params) {
+        super(params);
+        this.baseUrl = 'https://api.anapi.com';
+
+        this.URLs = {
+            authorization: '/oauth/authorize',
+            access_token: '/oauth/v1/token',
+        };
+
+        this.authorizationUri = encodeURI(
+            `https://app.anapi.com/oauth/authorize?client_id=${this.client_id}&redirect_uri=${this.redirect_uri}&scope=${this.scope}&state=${this.state}`
+        );
+        this.tokenUri = 'https://api.anapi.com/oauth/v1/token';
+
+        this.access_token = get(params, 'access_token', null);
+        this.refresh_token = get(params, 'refresh_token', null);
+    }
+    getAuthUri() {
+        return this.authorizationUri;
+    }
+
+
+}
+
+module.exports = { Api };
