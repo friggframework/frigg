aa65a38c44f6651f634b9d034540030d0015c454
michael webber <michaelryanwebber@gmail.com>
2024-03-22 14:13:36 -0400

    set test environment STAGE to dev

--- packages/devtools/test-environment/jest-global-setup.js
+++ packages/devtools/test-environment/jest-global-setup.js
@@ -1,6 +1,8 @@
 const { TestMongo } = require('./mongodb');
+const { overrideEnvironment} = require('./override-environment');
 
 module.exports = async function () {
+    overrideEnvironment({ STAGE: 'dev'})
     global.testMongo = new TestMongo();
     await global.testMongo.start();
 };
