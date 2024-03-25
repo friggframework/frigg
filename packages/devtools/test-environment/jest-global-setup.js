9d899861cd56bdd9b9aedea02c4195d9e3fe1214
michael webber <michaelryanwebber@gmail.com>
2024-03-25 13:26:47 -0400

    Revert "set test environment STAGE to dev"
    This reverts commit aa65a38c44f6651f634b9d034540030d0015c454.

--- packages/devtools/test-environment/jest-global-setup.js
+++ packages/devtools/test-environment/jest-global-setup.js
@@ -1,8 +1,6 @@
 const { TestMongo } = require('./mongodb');
-const { overrideEnvironment} = require('./override-environment');
 
 module.exports = async function () {
-    overrideEnvironment({ STAGE: 'dev'})
     global.testMongo = new TestMongo();
     await global.testMongo.start();
 };
