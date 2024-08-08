4bf9c6742f9206e4ec2b66e5148237f25d53e863
d-klotz <danielfelipeklotz@gmail.com>
2024-08-08 13:08:04 -0300

    chore: misc

--- packages/ui/vite.config.js
+++ packages/ui/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, "./lib/index.js"),
-      name: "react-beautiful-timeline",
+      name: "friggframework-ui",
       fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
