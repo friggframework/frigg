68fde9687d921c41f354af3618155b5219d4d8e0
Sean Matthews <sean.matthews@lefthook.co>
2024-08-19 17:18:25 -0400

    WIP trying to work out programmatic serverless. Webpack is struggling to get the local linked package built right. Might be a non-issue, tbd.

---
+++ packages/devtools/infrastructure/backend-utils.js
@@ -0,0 +1,24 @@
+const { createFriggBackend } = require('@friggframework/core');
+const { findNearestBackendPackageJson } = require('../frigg-cli/utils/backend-path');
+const path = require('path');
+const fs = require('fs-extra');
+
+const backendPath = findNearestBackendPackageJson();
+if (!backendPath) {
+    throw new Error('Could not find backend package.json');
+}
+
+const backendDir = path.dirname(backendPath);
+const backendFilePath = path.join(backendDir, 'backend.js');
+if (!fs.existsSync(backendFilePath)) {
+    throw new Error('Could not find backend.js');
+}
+
+const backendJsFile = require(backendFilePath);
+const appDefinition = backendJsFile.Definition;
+
+const backend = createFriggBackend(appDefinition);
+
+module.exports = {
+    ...backend,
+};
