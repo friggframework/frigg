6d3780071cb8019daba77e8e1c373472b3774210
Sean Matthews <sean.matthews@lefthook.co>
2024-10-16 22:54:34 -0400

    Added helper functions and migrated from prior setup (basically, lots of renaming) for frigg-cli directory structure.

---
+++ packages/devtools/frigg-cli/start-command/index.js
@@ -0,0 +1,30 @@
+const { spawn } = require('child_process');
+const path = require('path');
+
+function startCommand() {
+    console.log('Starting backend and optional frontend...');
+    // Suppress AWS SDK warning message about maintenance mode
+    process.env.AWS_SDK_JS_SUPPRESS_MAINTENANCE_MODE_MESSAGE = 1;
+    const backendPath = path.resolve(process.cwd());
+    console.log(`Starting backend in ${backendPath}...`);
+    const infrastructurePath = 'infrastructure.js';
+    const command = 'serverless';
+    const args = ['offline', '--config', infrastructurePath, '--stage=dev'];
+
+    const childProcess = spawn(command, args, {
+        cwd: backendPath,
+        stdio: 'inherit',
+    });
+
+    childProcess.on('error', (error) => {
+        console.error(`Error executing command: ${error.message}`);
+    });
+
+    childProcess.on('close', (code) => {
+        if (code !== 0) {
+            console.log(`Child process exited with code ${code}`);
+        }
+    });
+}
+
+module.exports = { startCommand };
