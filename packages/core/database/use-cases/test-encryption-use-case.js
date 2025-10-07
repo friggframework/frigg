4c72357ce52149d1f4fcaec73fc28b0309e63efb
d-klotz <danielfelipeklotz@gmail.com>
2025-10-07 18:23:56 -0300

    feat: update test data mapping to use camelCase for Prisma compatibility

--- packages/core/database/use-cases/test-encryption-use-case.js
+++ packages/core/database/use-cases/test-encryption-use-case.js
@@ -83,13 +83,14 @@ class TestEncryptionUseCase {
      * @private
      */
     _mapTestDataToCredential(testData) {
+        // Note: Using camelCase for Prisma compatibility (both MongoDB and PostgreSQL)
+        // Changed from snake_case (user_id, entity_id) to camelCase (userId, externalId)
         return {
-            user_id: 'test-encryption-user',
-            entity_id: 'test-encryption-entity',
+            externalId: 'test-encryption-entity',
             data: {
-                access_token: testData.testSecret,
-                refresh_token: testData.nestedSecret?.value,
-                domain: testData.normalField,
+                access_token: testData.testSecret,      // Encrypted field
+                refresh_token: testData.nestedSecret?.value, // Encrypted field
+                domain: testData.normalField,           // Not encrypted
             },
         };
     }
