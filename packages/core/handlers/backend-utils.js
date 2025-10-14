48646e1aae02ffe5d20b005944d5744b64f8ba1e
d-klotz <danielfelipeklotz@gmail.com>
2025-10-14 18:30:30 -0300

    feat: enhance QueueWorker to create repositories and handle integration instances

--- packages/core/handlers/backend-utils.js
+++ packages/core/handlers/backend-utils.js
@@ -3,6 +3,12 @@ const { Worker } = require('@friggframework/core');
 const {
     IntegrationEventDispatcher,
 } = require('./integration-event-dispatcher');
+const { GetIntegrationInstance } = require('../integrations/use-cases/get-integration-instance');
+const { ModuleFactory } = require('../modules/module-factory');
+const { createProcessRepository } = require('../integrations/repositories/process-repository-factory');
+const { createIntegrationRepository } = require('../integrations/repositories/integration-repository-factory');
+const { createModuleRepository } = require('../modules/repositories/module-repository-factory');
+const { getModulesDefinitionFromIntegrationClasses } = require('../integrations/utils/map-integration-dto');
 
 const loadRouterFromObject = (IntegrationClass, routerObject) => {
     const router = Router();
@@ -37,7 +43,48 @@ const createQueueWorker = (integrationClass) => {
     class QueueWorker extends Worker {
         async _run(params, context) {
             try {
-                const integrationInstance = new integrationClass();
+                // Create repositories
+                const processRepository = createProcessRepository();
+                const integrationRepository = createIntegrationRepository();
+                const moduleRepository = createModuleRepository();
+
+                // Extract module definitions from integration class (as array)
+                const moduleDefinitions = getModulesDefinitionFromIntegrationClasses([integrationClass]);
+
+                // Create module factory
+                const moduleFactory = new ModuleFactory({
+                    moduleRepository,
+                    moduleDefinitions,
+                });
+
+                // Create GetIntegrationInstance use case
+                const getIntegrationInstance = new GetIntegrationInstance({
+                    integrationRepository,
+                    integrationClasses: [integrationClass],
+                    moduleFactory,
+                });
+
+                // Get processId from queue message data
+                const processId = params.data?.processId;
+
+                if (!processId) {
+                    throw new Error('processId is required in queue message data');
+                }
+
+                // Look up process to get integrationId and userId
+                const process = await processRepository.findById(processId);
+
+                if (!process) {
+                    throw new Error(`Process not found: ${processId}`);
+                }
+
+                // Load integration instance with modules
+                const integrationInstance = await getIntegrationInstance.execute(
+                    process.integrationId,
+                    process.userId
+                );
+
+                // Dispatch event to integration handler
                 const dispatcher = new IntegrationEventDispatcher(
                     integrationInstance
                 );
