45e06e556cdc386d97ccd8989a234f0c1bf4370c
d-klotz <danielfelipeklotz@gmail.com>
2026-03-05 14:39:48 -0300

    refactor(core): remove unused Entity import

--- packages/core/modules/index.js
+++ packages/core/modules/index.js
@@ -1,4 +1,3 @@
-const { Entity } = require('./entity');
 const { ApiKeyRequester } = require('./requester/api-key');
 const { BasicAuthRequester } = require('./requester/basic');
 const { OAuth2Requester } = require('./requester/oauth-2');
@@ -7,7 +6,6 @@ const { ModuleConstants } = require('./ModuleConstants');
 const { ModuleFactory } = require('./module-factory');
 
 module.exports = {
-    Entity,
     ApiKeyRequester,
     BasicAuthRequester,
     OAuth2Requester,
